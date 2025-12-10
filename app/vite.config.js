a1d0a05ad39cbada6f96176558e0b7c90975967a
AllanRoz <arozadev@gmail.com>
2025-12-10 09:30:59 -0500

    vite config changed

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NJIT-CS351-RSA-Algorithm/",
+  base: "/NJIT-CS351-RSA-Algorithm",
 })
